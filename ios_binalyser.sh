06dfaf85e4cbc3c91a35b25b0550f6e6fcc35b7d
fuzzKitty <shlomo@fuzzkitty.com>
2019-05-13 12:59:04 +0300

    Update ios_binalyser.sh

--- ios_binalyser.sh
+++ ios_binalyser.sh
@@ -3,10 +3,18 @@
 name="$(echo $1 | sed 's/\.[^.]*$//')"
 echo "Binary file to analyse: $name"
 
+output_dir=/private/var/tmp/"$name"/
+mkdir $output_dir
+chmod 777 $output_dir
+
+###FUNCTIONS###
 otool_aslr=$(otool -Vh "${name}")
 otool_ssp=$(otool -I -v "${name}" | grep stack_chk_guard)
 otool_arc=$(otool -I -v "${name}" | grep _objc_release)
 
+strings_full=$(strings "${name}" > /private/var/tmp/"$name"/strings_full.txt)
+
+###EXECUTION###
 echo -e "\n[1] Checking for Address Space Layout Randomization (ASLR) PIE flag...\n"
 echo -e "$otool_aslr\n\n"
 
@@ -15,3 +23,7 @@ echo -e "$otool_ssp\n\n"
 
 echo -e "\n[3] Checking for Automatic Reference Counting (ARC)...\n"
 echo -e "$otool_arc\n\n"
+
+echo -e "\n[4] Extracting all strings to "$output_dir"...\n"
+echo -e "$strings_full\n\n"
+
